5937409f6faf71dd8a71118d13d6996e71c5ab59
CKIN7 <covain77@gmail.com>
2024-04-03 15:25:05 -0400

    selectedLabels

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,11 +1,15 @@
 import { useLabels } from "../../hooks/useLabels"
 import { LoadingIcon } from "../../shared/components/LoadingIcon"
+import { FC } from 'react'
 
 
+interface Props {
+  selectedLabels: string[]
+  onChange: (labelName: string) => void
+}
 
 
-
-export const LabelPicker = () => {
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange}) => {
 
   const labelsQuery = useLabels()
 
@@ -15,9 +19,10 @@ export const LabelPicker = () => {
       {
         labelsQuery.data?.map( label => (
           <span 
-            className="badge rounded-pill m-1 label-picker"
+            className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? 'label-active' : ''}`}
             key={label.id}
             style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+            onClick={() => onChange(label.name)}
         >
             {label.name}
         </span>
