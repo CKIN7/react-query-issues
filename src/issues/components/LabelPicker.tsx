49fca7073d356d50fa7d575893fdd924e39af21d
CKIN7 <covain77@gmail.com>
2024-04-03 15:03:02 -0400

    Custom Hook useLabels, LoadingIcon, sleep y Placeholder Initialdata

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,28 +1,28 @@
-import { useQuery } from "@tanstack/react-query"
-import { gitHubApi } from "../../api/gitHubApi"
-import { Label } from "../../interfaces/label"
+import { useLabels } from "../../hooks/useLabels"
+import { LoadingIcon } from "../../shared/components/LoadingIcon"
 
-const getLabels = async():Promise<Label[]> => {
-  const { data } = await gitHubApi.get<Label[]>('/labels')
-  return data
-}
+
+
 
 
 export const LabelPicker = () => {
 
-  const labelsQuery = useQuery({
-    queryKey: ['labels'],
-    queryFn: getLabels,
-  })
+  const labelsQuery = useLabels()
 
+  if(labelsQuery.isLoading) return <LoadingIcon />
   return (
     <div>
-        <span 
+      {
+        labelsQuery.data?.map( label => (
+          <span 
             className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+            key={label.id}
+            style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
         >
-            Primary
+            {label.name}
         </span>
+        ))
+      }
         
     </div>
   )
