87cc2ecf76724259b1a39df08d802954b74e916a
CKIN7 <covain77@gmail.com>
2024-04-04 14:26:05 -0400

    Navegar y cargar issue por numero

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const gitHubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        
+      Authorization:  'Bearer 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'
     }
 })
